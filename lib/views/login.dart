80cec9ccb39dd285fc69bac4fae44a8d84dada02
Dalex19 <dearalex875@gmail.com>
2023-02-19 22:40:58 -0600

    add first screen

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -8,11 +8,99 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Ingresar', textAlign: TextAlign.center),
       ),
-      body: const Center(
-        child: Text('Hello word'),
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 170,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset('assets/login-bg.png',
+                fit: BoxFit.cover, alignment: Alignment.center),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: BodyLogin(),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class BodyLogin extends StatefulWidget {
+  @override
+  State<BodyLogin> createState() => _BodyLoginState();
+}
+
+class _BodyLoginState extends State<BodyLogin> {
+  bool obscure = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.50,
+        padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+        //color: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Login',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                )
+              ],
+            ),
+            TextFormField(
+              //controller: ,
+              decoration: const InputDecoration(
+                  label: Text('Email'),
+                  hintText: 'Your email ID'
+              ),
+            ),
+            TextFormField(
+              //controller: ,
+              obscureText: obscure,
+              decoration: InputDecoration(
+                  label: const Text('Password'),
+                  hintText: 'Password',
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          obscure = !obscure;
+                        });
+                      }, 
+                  icon: const Icon(Icons.remove_red_eye)
+                  )
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Text('Forget Password?', style: TextStyle(fontSize: 14))
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              padding: const EdgeInsets.symmetric(horizontal: 60),
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Colors.grey),
+              alignment: Alignment.center,
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+            ),
+            const Text('Dont have an account? Sign Up',
+                textAlign: TextAlign.center)
+          ],
+        ));
   }
 }
