0a2e2a58ece8fde05e2feec37153ac83d912375f
Dalex19 <dearalex875@gmail.com>
2023-02-21 22:32:20 -0600

    models for router

---
+++ lib/models/menu_options.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/cupertino.dart';
+
+class MenuOption {
+ final String route;
+ final String name;
+ final Widget screen;
+
+  MenuOption({required this.route, required this.name, required this.screen});
+
+}
