7ea19d1496a2628c288d2e7728ac581ec057c24c
Dalex19 <dearalex875@gmail.com>
2023-02-21 22:31:19 -0600

    screes template

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
-class Register extends StatelessWidget {
-  const Register({Key? key}) : super(key: key);
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
