8e31433df93d498c0a0b7d52bba060ad48c903b1
Dalex19 <dearalex875@gmail.com>
2023-02-21 22:26:15 -0600

    fix styles, and rename class

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +13,7 @@ class Login extends StatelessWidget {
       ),
       resizeToAvoidBottomInset: false,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           SizedBox(
             height: 170,
@@ -19,10 +21,7 @@ class Login extends StatelessWidget {
             child: Image.asset('assets/login-bg.png',
                 fit: BoxFit.cover, alignment: Alignment.center),
           ),
-          Align(
-            alignment: Alignment.center,
-            child: BodyLogin(),
-          ),
+           BodyLogin()
         ],
       ),
     );
@@ -40,7 +39,7 @@ class _BodyLoginState extends State<BodyLogin> {
   Widget build(BuildContext context) {
     return Container(
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.50,
+        height: MediaQuery.of(context).size.height * 0.60,
         padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
         //color: Colors.red,
         child: Column(
@@ -85,21 +84,43 @@ class _BodyLoginState extends State<BodyLogin> {
                 Text('Forget Password?', style: TextStyle(fontSize: 14))
               ],
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 40,
-              padding: const EdgeInsets.symmetric(horizontal: 60),
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Colors.grey),
-              alignment: Alignment.center,
-              child: const Text(
-                'Login',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            MaterialButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)
               ),
+              disabledColor: Colors.black12,
+                color: Colors.deepPurpleAccent,
+              elevation: 2.5,
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child:const Text('Login',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400
+                  )
+                ),
+              )
             ),
-            const Text('Dont have an account? Sign Up',
-                textAlign: TextAlign.center)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  child: Text('Dont have an account?'),
+                ),
+                SizedBox(
+                  child: TextButton(
+                      onPressed: () => Navigator.pushReplacementNamed(context, 'register'),
+                      child: Text(' Sign Up',
+                        style: TextStyle(
+                            color: Colors.deepPurpleAccent
+                        ),
+                      ),)
+                )
+              ],
+            )
           ],
         ));
   }
